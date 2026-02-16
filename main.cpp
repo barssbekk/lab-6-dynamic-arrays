1ad75d1a1ebdc23c6207d22fb778fa80adc68a96
Barsbek Azamatov <bars.moderside@gmail.com>
2026-02-15 17:08:58 -0800

    Add ouputArrayData()

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 void enterArrayData(double* arr, int size);
+void outputArrayData(const double* arr, int size);
 
 int main() {
     const int SIZE{5};
 
     double* data{new double[SIZE]};
     enterArrayData(data, SIZE);
+    outputArrayData(data, SIZE);
 
     delete[] data;
     return 0;
@@ -22,7 +24,12 @@ void enterArrayData(double *arr, int size) {
         cin >> userInput;
         *(arr + i) = userInput;
     }
-
-    for (int i{0}; i < size; ++i)
-        cout << *(arr + i);
+    cout << "Data entry complete.\n";
 }
+
+void outputArrayData(const double* arr, int size) {
+    cout << "Outputting array elements: ";
+    for (int i{0}; i < size; ++i)
+        cout << *(arr + i) << " ";
+    cout << '\n';
+};
