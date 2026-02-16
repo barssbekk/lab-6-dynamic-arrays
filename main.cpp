b27e0ab3b8a0a46554785e4326a3bc44672f0b40
Barsbek Azamatov <bars.moderside@gmail.com>
2026-02-15 16:36:02 -0800

    Declare enterArrayData()

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 #include <iostream>
 using namespace std;
 
+void enterArrayData(double* arr);
 int main() {
 
     return 0;
+}
+
+void enterArrayData(double *arr) {
+    cout << "Data entry for the array:";
+    // for (int i{0}; i < )
 }
