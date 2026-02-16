88c6d2471fc90f7f018aea0116dc214a8ff88d86
Barsbek Azamatov <bars.moderside@gmail.com>
2026-02-15 16:49:46 -0800

    Add second parameter

--- main.cpp
+++ main.cpp
@@ -2,16 +2,22 @@
 #include <iostream>
 using namespace std;
 
-void enterArrayData(double* arr);
+void enterArrayData(double* arr, int size); //
 
 int main() {
     const int SIZE{5};
-    double* data{new double};
-    delete data;
+
+    double* data{new double[SIZE]};
+    enterArrayData(data, SIZE);
+
+    delete[] data;
     return 0;
 }
 
-void enterArrayData(double *arr) {
+void enterArrayData(double *arr, int size) {
     cout << "Data entry for the array:";
-    // for (int i{0}; i < )
+    for (int i{0}; i < size; ++i) {
+        
+    }
+
 }
